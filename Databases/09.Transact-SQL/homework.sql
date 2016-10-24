8adcda3c7cea4264f79bba464aeed4cc1a8d6510
kalostoykov <kalo.stoykov@gmail.com>
2016-10-24 22:44:48 +0300

    task 2 - done

--- Databases/09.Transact-SQL/homework.sql
+++ Databases/09.Transact-SQL/homework.sql
@@ -1,4 +1,4 @@
--- task 1 Create a database with two tables: Persons(Id(PK), FirstName, LastName, SSN) and Accounts(Id(PK), PersonId(FK), Balance).
+-- task 1: Create a database with two tables: Persons(Id(PK), FirstName, LastName, SSN) and Accounts(Id(PK), PersonId(FK), Balance).
 	--Insert few records for testing.
     --Write a stored procedure that selects the full names of all persons.
 
@@ -44,3 +44,21 @@ AS
 GO
 
 EXEC usp_GetFullName
+
+GO
+
+-- task 2: Create a stored procedure that accepts a number as a parameter and 
+--returns all persons who have more money in their accounts than the supplied number.
+CREATE PROCEDURE usp_PersonsWithBalanceHigherThen(@amount money = 0)
+AS
+	SELECT p.FirstName + ' ' + p.LastName AS [Full name]
+	FROM Persons AS p
+	LEFT JOIN Accounts AS a
+	ON p.Id = a.PersonId
+	WHERE a.Balance > @amount
+
+GO
+
+EXEC usp_PersonsWithBalanceHigherThen 43
+
+GO
