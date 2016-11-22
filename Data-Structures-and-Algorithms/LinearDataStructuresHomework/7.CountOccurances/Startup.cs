e2dbd9fb8b467edeb07da399efb806879c8ed0ad
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-22 22:17:55 +0200

    task 7 - count occurances - done

---
+++ Data-Structures-and-Algorithms/LinearDataStructuresHomework/7.CountOccurances/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.CountOccurances
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> occuranceStorage = new Dictionary<int, int>();
+
+            List<int> sequence = new List<int>() { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+
+            foreach (int number in sequence)
+            {
+                if (occuranceStorage.ContainsKey(number))
+                {
+                    occuranceStorage[number]++;
+                }
+                else
+                {
+                    occuranceStorage[number] = 1;
+                }
+            }
+
+            foreach (int key in occuranceStorage.Keys)
+            {
+                Console.WriteLine($"Number: {key} -> {occuranceStorage[key]} times");
+            }
+
+        }
+    }
+}
