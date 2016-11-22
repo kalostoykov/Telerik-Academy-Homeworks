36cc1bf625df02744a66436df3be67d51ed82848
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-22 23:44:27 +0200

    task 9 - print sequence - done

---
+++ Data-Structures-and-Algorithms/LinearDataStructuresHomework/9.PrintSequenceMembers/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.PrintSequenceMembers
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            int n = int.Parse(Console.ReadLine());
+            int iterations = 50;
+
+            queue.Enqueue(n);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                Console.Write(queue.Peek() + ", ");
+
+                var s = queue.Dequeue();
+
+                queue.Enqueue(s + 1);
+                queue.Enqueue((2 * n) + 1);
+                queue.Enqueue(s + 2);
+                
+            }
+        }
+    }
+}
