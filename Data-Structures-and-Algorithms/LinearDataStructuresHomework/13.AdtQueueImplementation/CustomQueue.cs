0fa0ddf806519cfd9d470dee51c578212d3f7967
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-23 23:43:10 +0200

    AdtQueue implementation

---
+++ Data-Structures-and-Algorithms/LinearDataStructuresHomework/13.AdtQueueImplementation/CustomQueue.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace _13.AdtQueueImplementation
+{
+    class CustomQueue<T>
+    {
+		private LinkedList<T> queue;
+
+        public int Count { get; private set; }
+
+        public CustomQueue()
+		{
+			this.queue = new LinkedList<T>();
+		}
+
+		public void Enque(T item)
+		{
+            this.queue.AddLast(item);
+            this.Count++;
+		}
+
+        public T Dequeu()
+        {
+            var item = this.Peek();
+            this.queue.RemoveFirst();
+            this.Count--;
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            return this.queue.First.Value;
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/LinearDataStructuresHomework/13.AdtQueueImplementation/Statup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.AdtQueueImplementation
+{
+    class Statup
+    {
+        static void Main(string[] args)
+        {
+            var queue = new CustomQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enque(i);
+            }
+
+            while (queue.Count != 0)
+            {
+                Console.WriteLine(queue.Dequeu());
+            }
+        }
+    }
+}
