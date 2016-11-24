f179052f65f579f2405c86b9987c97b1b765dc48
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-24 10:55:16 +0200

    add Recussion homework plus nasted loops task

---
+++ Data-Structures-and-Algorithms/RecursionHomework/NastedLoopsSimulation/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NastedLoopsSimulation
+{
+    class Startup
+    {
+        static int n;
+
+        static int[] loops;
+
+        static void Main()
+        {
+            Console.Write("N = ");
+            n = int.Parse(Console.ReadLine());
+            loops = new int[n];
+
+            NestedLoops(0);
+        }
+
+        static void NestedLoops(int currentLoop)
+        {
+            if (currentLoop == n)
+            {
+                PrintLoops();
+                return;
+            }
+            for (int counter = 1; counter <= n; counter++)
+            {
+                loops[currentLoop] = counter;
+                NestedLoops(currentLoop + 1);
+            }
+        }
+
+        static void PrintLoops()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", loops[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
