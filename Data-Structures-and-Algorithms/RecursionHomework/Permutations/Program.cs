a350ed436420fd925a526d7d7d2f4ab9bd462a77
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-26 22:27:16 +0200

    permutations

---
+++ Data-Structures-and-Algorithms/RecursionHomework/Permutations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Permutations(n, new List<int>());
+        }
+
+        static void Permutations(int n, List<int> permutation)
+        {
+            if (permutation.Count == n)
+            {
+                Console.WriteLine(string.Join(", ", permutation));
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (permutation.Contains(i))
+                {
+                    continue;
+                }
+
+                permutation.Add(i);
+                Permutations(n, permutation);
+                permutation.RemoveAt(permutation.Count - 1);
+            }
+        }
+    }
+}
