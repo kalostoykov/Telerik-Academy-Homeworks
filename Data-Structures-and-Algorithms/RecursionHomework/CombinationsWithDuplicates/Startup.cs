662468d18e491923d77573d575de94e696cb0db4
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2016-11-26 21:44:41 +0200

    Combinations with repetition

---
+++ Data-Structures-and-Algorithms/RecursionHomework/CombinationsWithDuplicates/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsWithDuplicates
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            int[] numbers = new int[k];
+
+            Combinations(numbers, 0, n, 1);
+        }
+
+        private static void Combinations(int[] numbers, int currentIndex, int n, int value)
+        {
+            if (currentIndex == numbers.Length)
+            {
+                Console.WriteLine(string.Join(", ", numbers));
+                return;
+            }
+
+            for (int i = value; i <= n; i++)
+            {
+                numbers[currentIndex] = i;
+                Combinations(numbers, currentIndex + 1, n, i);
+            }
+        }
+    }
+}
