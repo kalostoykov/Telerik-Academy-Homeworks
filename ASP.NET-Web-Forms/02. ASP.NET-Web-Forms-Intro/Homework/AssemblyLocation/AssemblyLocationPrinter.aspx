88679daf711aec9c28837545dff07dff934278bd
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-01-23 16:34:00 +0200

    assembly location - done

---
+++ ASP.NET-Web-Forms/02. ASP.NET-Web-Forms-Intro/Homework/AssemblyLocation/AssemblyLocationPrinter.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AssemblyLocationPrinter.aspx.cs" Inherits="AssemblyLocation.AssemblyLocationPrinter" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="mainForm" runat="server">
+    <div>
+        <label id="LabelHello" runat="server" />
+    </div>
+    <div>
+        <label id="LabelAsseblyLocation" runat="server"  text=""/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Web-Forms/02. ASP.NET-Web-Forms-Intro/Homework/AssemblyLocation/AssemblyLocationPrinter.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AssemblyLocation
+{
+    public partial class AssemblyLocationPrinter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.LabelHello.InnerText = "Hello, ASP.NET";
+            this.LabelAsseblyLocation.InnerText += "Assembly location: " + Assembly.GetExecutingAssembly().Location;
+        }
+    }
+}
