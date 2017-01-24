62e2894d90a8c9cb76ed8bc8f522c04c48762c51
Kaloyan Stoykov <kalo.stoykov@gmail.com>
2017-01-24 16:42:43 +0200

    Students and courses form task - done

---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-Controls-and-HTML-Controls/Homework/StudentsAndCourses/Register.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="StudentsAndCourses.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="registerForm" runat="server">
+    <div id="register">
+        <asp:TextBox ID="TextBoxFirstName" runat="server" PlaceHolder="First name" />
+        <asp:TextBox ID="TextBoxLastName" runat="server" PlaceHolder="Last name" />
+        <asp:TextBox ID="TextBoxFacultyNumber" runat="server" PlaceHolder="Faculty number"/>
+        <asp:DropDownList ID="DropDownListUniversity" runat="server">
+            <asp:ListItem Text="SU" />
+            <asp:ListItem Text="TU" />
+            <asp:ListItem Text="UNIBIT" />
+        </asp:DropDownList>
+        <asp:DropDownList ID="DropDownListSpecialty" runat="server">
+            <asp:ListItem Text="CS" />
+            <asp:ListItem Text="IT" />
+        </asp:DropDownList>
+        <asp:ListBox ID="ListBoxCourses" runat="server" SelectionMode="Multiple">
+            <asp:ListItem Text="Math" />
+            <asp:ListItem Text="CSharp" />
+            <asp:ListItem Text="DSA" />
+        </asp:ListBox>
+        <asp:Button ID="ButtonSubmit" runat="server" Text="Submit" OnClick="ButtonSubmit_Click"/>
+    </div>
+    <hr />
+    <div id="registerInfo" runat="server"></div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET-Web-Forms/03. ASP.NET-Web-Controls-and-HTML-Controls/Homework/StudentsAndCourses/Register.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace StudentsAndCourses
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            var nameHeader = new HtmlGenericControl("h1");
+            nameHeader.InnerText = string.Format("Name: {0} {1}", this.TextBoxFirstName.Text, this.TextBoxLastName.Text);
+
+            var facultyNumberHeader = new HtmlGenericControl("h1");
+            facultyNumberHeader.InnerText = string.Format("Faculty number: {0}", this.TextBoxFacultyNumber.Text);
+
+            var universityHeader = new HtmlGenericControl("h1");
+            universityHeader.InnerText = string.Format("University: {0}", this.DropDownListUniversity.SelectedValue);
+
+            var specialtyHeader = new HtmlGenericControl("h1");
+            specialtyHeader.InnerText = string.Format("Specialty: {0}", this.DropDownListSpecialty.SelectedValue);
+
+            var coursesHeader = new HtmlGenericControl("h1");
+            coursesHeader.InnerText = "Courses: ";
+
+            var courses = new BulletedList();
+
+            foreach (ListItem course in this.ListBoxCourses.Items)
+            {
+                if (course.Selected)
+                {
+                    courses.Items.Add(course);
+                }
+            }
+
+            this.registerInfo.Controls.Add(nameHeader);
+            this.registerInfo.Controls.Add(facultyNumberHeader);
+            this.registerInfo.Controls.Add(universityHeader);
+            this.registerInfo.Controls.Add(coursesHeader);
+            this.registerInfo.Controls.Add(courses);
+        }
+    }
+}
